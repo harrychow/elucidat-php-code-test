9bb88c4eaffaedcd0f3f659910279d713fca6e7a
Harry Chow <h.chow@reply.com>
2021-04-12 23:02:10 +0100

    Refactor: Moved magic numbers and strings into constants

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -5,6 +5,11 @@ namespace App;
 class GildedRose
 {
     private $items;
+    const MAX_QUALITY = 50;
+    const MIN_QUALITY = 0;
+    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+    const BRIE = 'Aged Brie';
+    const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
 
     public function __construct(array $items)
     {
@@ -22,37 +27,37 @@ class GildedRose
     public function nextDay()
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
+            if ($item->name != self::BRIE and $item->name != self::BACKSTAGE_PASSES) {
+                if ($item->quality > self::MIN_QUALITY) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
-                if ($item->quality < 50) {
+                if ($item->quality < self::MAX_QUALITY) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->name == self::BACKSTAGE_PASSES) {
                         if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
+                            if ($item->quality < self::MAX_QUALITY) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
                         if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
+                            if ($item->quality < self::MAX_QUALITY) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
                     }
                 }
             }
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name !== self::SULFURAS) {
                 $item->sellIn = $item->sellIn - 1;
             }
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->sellIn < self::MIN_QUALITY) {
+                if ($item->name != self::BRIE) {
+                    if ($item->name != self::BACKSTAGE_PASSES) {
+                        if ($item->quality > self::MIN_QUALITY) {
+                            if ($item->name != self::SULFURAS) {
                                 $item->quality = $item->quality - 1;
                             }
                         }
@@ -60,7 +65,7 @@ class GildedRose
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
+                    if ($item->quality < self::MAX_QUALITY) {
                         $item->quality = $item->quality + 1;
                     }
                 }
