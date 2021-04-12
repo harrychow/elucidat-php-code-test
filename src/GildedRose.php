43d3f02804cfa8a8330c1efe479674ddffe34ed5
Harry Chow <h.chow@reply.com>
2021-04-12 23:32:20 +0100

    Refactor: Use strict comparison for string compare.  use shorthand increment/decrement

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,34 +24,45 @@ class GildedRose
         );
     }
 
+    private function isLegendary($item) {
+        return $item->name === self::SULFURAS;
+    }
+
+    private function updateSellin($item) {
+
+    }
+
+    private function updateQuality($item) {
+
+    }
+
     public function nextDay()
     {
         foreach ($this->items as $item) {
-            if ($item->name != self::BRIE and $item->name != self::BACKSTAGE_PASSES) {
-                if ($item->quality > self::MIN_QUALITY) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
+            $this->updateSellin($item);
+            $this->updateQuality($item);
+
+            if ($item->name !== self::BRIE && $item->name !== self::BACKSTAGE_PASSES) {
+                if (($item->quality > self::MIN_QUALITY) && !$this->isLegendary($item)) {
+                    --$item->quality;
                 }
-            } else {
-                if ($item->quality < self::MAX_QUALITY) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == self::BACKSTAGE_PASSES) {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < self::MAX_QUALITY) {
-                                $item->quality = $item->quality + 1;
-                            }
+            } else if ($item->quality < self::MAX_QUALITY) {
+                ++$item->quality;
+                if ($item->name === self::BACKSTAGE_PASSES) {
+                    if ($item->sellIn < 11) {
+                        if ($item->quality < self::MAX_QUALITY) {
+                            ++$item->quality;
                         }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < self::MAX_QUALITY) {
-                                $item->quality = $item->quality + 1;
-                            }
+                    }
+                    if ($item->sellIn < 6) {
+                        if ($item->quality < self::MAX_QUALITY) {
+                            ++$item->quality;
                         }
                     }
                 }
             }
-            if ($item->name !== self::SULFURAS) {
-                $item->sellIn = $item->sellIn - 1;
+            if (!$this->isLegendary($item)) {
+                --$item->sellIn;
             }
             if ($item->sellIn < self::MIN_QUALITY) {
                 if ($item->name != self::BRIE) {
